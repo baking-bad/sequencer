8c43c302e2dd9c062f3e61960782abd9f431301e
Michael Zaikin <mz@pklab.io>
2023-12-25 18:08:32 +0200

    Consensus client for sending txs

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -1,29 +1,38 @@
 use clap::Parser;
+use consensus_client::ConsensusClient;
 use fastcrypto::hash::{HashFunction, Keccak256};
+use tide::StatusCode;
 use std::sync::Arc;
 
 mod da_batcher;
 mod rollup_client;
+mod consensus_client;
 
 use rollup_client::RollupClient;
 
 #[derive(Clone)]
 struct State {
     pub rollup_client: Arc<RollupClient>,
+    pub worker_client: Arc<ConsensusClient>,
 }
 
 impl State {
-    pub fn new(rollup_node_url: String) -> Self {
+    pub fn new(rollup_node_url: String, worker_node_url: String) -> Self {
         Self {
             rollup_client: Arc::new(RollupClient::new(rollup_node_url)),
+            worker_client: Arc::new(ConsensusClient::new(worker_node_url)),
         }
     }
 }
 
 async fn broadcast_transaction(mut req: tide::Request<State>) -> tide::Result<String> {
-    // TODO: send to consensus node
-    let tx_payload = req.body_string().await?;
-    let tx_digest = Keccak256::digest(hex::decode(tx_payload)?);
+    let tx_payload = hex::decode(req.body_string().await?)?;
+    let tx_digest = Keccak256::digest(&tx_payload);
+    req.state()
+        .worker_client
+        .as_ref()
+        .clone() // cloning channel is cheap and encouraged
+        .send_transaction(tx_payload).await?;
     Ok(hex::encode(tx_digest))
 }
 
@@ -42,8 +51,8 @@ async fn get_block_by_level(req: tide::Request<State>) -> tide::Result<tide::Bod
     tide::Body::from_json(&res)
 }
 
-async fn run_api_server(rpc_host: String, rollup_node_url: String) -> tide::Result<()> {
-    let mut app = tide::with_state(State::new(rollup_node_url));
+async fn run_api_server(rpc_host: String, rollup_node_url: String, worker_node_url: String) -> tide::Result<()> {
+    let mut app = tide::with_state(State::new(rollup_node_url, worker_node_url));
     app.at("/broadcast").post(broadcast_transaction);
     app.at("/blocks/:level").get(get_block_by_level);
     app.listen(rpc_host).await?;
@@ -55,7 +64,10 @@ struct Args {
     #[arg(long, default_value_t = String::from("http://localhost:8932"))]
     rollup_node_url: String,
 
-    #[arg(long, default_value_t = String::from("127.0.0.1"))]
+    #[arg(long, default_value_t = String::from("http://localhost:9090"))]
+    worker_node_url: String,
+
+    #[arg(long, default_value_t = String::from("0.0.0.0"))]
     rpc_addr: String,
 
     #[arg(long, default_value_t = 8080)]
@@ -69,5 +81,5 @@ async fn main() {
     let args = Args::parse();
     let rpc_host = format!("http://{}:{}", args.rpc_addr, args.rpc_port);
 
-    tokio::try_join!(run_api_server(rpc_host, args.rollup_node_url)).unwrap();
+    tokio::try_join!(run_api_server(rpc_host, args.rollup_node_url, args.worker_node_url)).unwrap();
 }
