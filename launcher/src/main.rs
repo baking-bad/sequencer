1764194548671345949779f72fee7dfed07f4f88
Maksim Strebkov <257byte@gmail.com>
2023-12-30 17:42:48 +0300

    Simplify launcher

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,11 +1,7 @@
-use std::{env, thread};
+use std::env;
 use std::path::Path;
-use std::process::{Command, Child};
-use std::sync::{Arc, atomic::AtomicBool, atomic::Ordering};
-use std::time::Duration;
+use std::process::Command;
 use clap::Parser;
-use signal_hook::flag::register;
-use signal_hook::consts::{SIGINT, SIGTERM};
 
 /// Simple launcher that runs narwhal primary with a single worker
 /// with preconfigured parameters, keys, and committee
@@ -30,159 +26,32 @@ fn main() {
     std::env::set_current_dir(&repository_root.to_path_buf())
         .expect("Failed to set working directory");
 
-    // handle termination signals to properly stop primary and worker processes
-    let terminate = Arc::new(AtomicBool::new(false));
-    register(SIGINT, Arc::clone(&terminate))
-        .expect("Failed to register SIGINT handler");
-    register(SIGTERM, Arc::clone(&terminate))
-        .expect("Failed to register SIGTERM handler");
-
-    // // run primary
-    // let mut primary = run(NodeType::Primary, args.id, args.log_level)
-    //     .expect("Failed to run primary");
-
-    // println!("Primary started with pid {}", primary.id());
-
-    // // run worker
-    // let mut worker = run(NodeType::Worker, args.id, args.log_level)
-    //     .expect("Failed to run worker");
-
-    // println!("Worker started with pid {}", worker.id());
-
-    // run comb
-    let mut comb = run_comb(args.id, args.log_level)
-        .expect("Failed to run comb");
-
-    println!("Comb started with pid {}", comb.id());
-
-    // wait for termination
-    while !terminate.load(Ordering::Relaxed) {
-        // // check if primary is ok
-        // match primary.try_wait() {
-        //     Ok(None) => {
-        //         // primary is ok
-        //     },
-        //     Ok(Some(exit_code)) => {
-        //         println!("Primary exited with {}", exit_code);
-        //         break;
-        //     },
-        //     Err(e) => {
-        //         println!("Failed to check primary's status: {:?}", e);
-        //         break;
-        //     }
-        // }
-        // // check if worker is ok
-        // match worker.try_wait() {
-        //     Ok(None) => {
-        //         // worker is ok
-        //     },
-        //     Ok(Some(exit_code)) => {
-        //         println!("Worker exited with {}", exit_code);
-        //         break;
-        //     },
-        //     Err(e) => {
-        //         println!("Failed to check worker's status: {:?}", e);
-        //         break;
-        //     }
-        // }
-        // check if comb is ok
-        match comb.try_wait() {
-            Ok(None) => {
-                // comb is ok
-            },
-            Ok(Some(exit_code)) => {
-                println!("Comb exited with {}", exit_code);
-                break;
-            },
-            Err(e) => {
-                println!("Failed to check comb's status: {:?}", e);
-                break;
-            }
-        }
-        thread::sleep(Duration::from_millis(500));
-    }
-    
-    println!("Killing child processes...");
-
-    // primary.kill().expect("Failed to kill primary");
-    // worker.kill().expect("Failed to kill worker");
-    comb.kill().expect("Failed to kill worker");
-
-    // println!("Primary exited with {}", primary.wait().unwrap());
-    // println!("Worker exited with {}", worker.wait().unwrap());
-    println!("Comb exited with {}", comb.wait().unwrap());
-}
-
-// enum NodeType {
-//     Primary,
-//     Worker,
-// }
-
-// fn run(node_type: NodeType, id: u8, log_level: u8) -> std::io::Result<Child> {
-//     let mut cmd = Command::new("cargo");
-    
-//     cmd.args(vec![
-//         "run",
-//         "--bin", "narwhal-node",
-//         "--",
-//     ]);
-
-//     if log_level > 0 {
-//         let verbosity = format!("-{}", (0..log_level).map(|_| "v").collect::<String>());
-//         cmd.arg(verbosity);
-//     }
-
-//     cmd.args(vec![
-//         "run",
-//         "--primary-keys", format!("./launcher/defaults/primary-{}.key", id).as_str(),
-//         "--primary-network-keys", format!("./launcher/defaults/primary-network-{}.key", id).as_str(),
-//         "--worker-keys", format!("./launcher/defaults/worker-network-{}.key", id).as_str(),
-//         "--committee", "./launcher/defaults/committee.json",
-//         "--workers", "./launcher/defaults/workers.json",
-//     ]);
-
-//     match node_type {
-//         NodeType::Primary => {
-//             cmd.args(vec![
-//                 "--store", format!("./.db/primary-{}", id).as_str(),
-//                 "primary",
-//             ]);
-//         }
-//         NodeType::Worker => {
-//             cmd.args(vec![
-//                 "--store", format!("./.db/worker-{}", id).as_str(),
-//                 "worker", "0",
-//             ]);
-//         }
-//     }
-    
-//     cmd.spawn()
-// }
-
-fn run_comb(id: u8, log_level: u8) -> std::io::Result<Child> {
     let mut cmd = Command::new("cargo");
-    
+
     cmd.args(vec![
         "run",
         "--bin", "narwhal-node",
         "--",
     ]);
 
-    if log_level > 0 {
-        let verbosity = format!("-{}", (0..log_level).map(|_| "v").collect::<String>());
+    if args.log_level > 0 {
+        let verbosity = format!("-{}", (0..args.log_level).map(|_| "v").collect::<String>());
         cmd.arg(verbosity);
     }
 
     cmd.args(vec![
         "run-comb",
-        "--primary-keys", format!("./launcher/defaults/primary-{}.key", id).as_str(),
-        "--primary-network-keys", format!("./launcher/defaults/primary-network-{}.key", id).as_str(),
-        "--worker-keys", format!("./launcher/defaults/worker-network-{}.key", id).as_str(),
+        "--primary-keys", format!("./launcher/defaults/primary-{}.key", args.id).as_str(),
+        "--primary-network-keys", format!("./launcher/defaults/primary-network-{}.key", args.id).as_str(),
+        "--worker-keys", format!("./launcher/defaults/worker-network-{}.key", args.id).as_str(),
         "--committee", "./launcher/defaults/committee.json",
         "--workers", "./launcher/defaults/workers.json",
-        "--primary-store", format!("./.db/primary-{}", id).as_str(),
-        "--worker-store", format!("./.db/worker-{}", id).as_str(),
+        "--primary-store", format!("./.db/primary-{}", args.id).as_str(),
+        "--worker-store", format!("./.db/worker-{}", args.id).as_str(),
     ]);
     
-    cmd.spawn()
+    // run and wait for exit
+    let status = cmd.status();
+
+    println!("Node exited with {:?}", status);
 }
