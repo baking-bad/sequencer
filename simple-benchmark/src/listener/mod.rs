9aea483b06cadc50c4fff59e0eda222f5ff45fcb
Andrea Cerone <andrea.cerone@trili.tech>
2024-01-29 11:55:22 +0000

    Simple benchmark: restructure files

---
+++ simple-benchmark/src/listener/mod.rs
@@ -0,0 +1,77 @@
+use log::error;
+use log::info;
+use tokio::time::sleep;
+use tonic::transport::Channel;
+
+use super::exporter::exporter_client::ExporterClient;
+use super::exporter::*;
+use super::ListenerArgs;
+use std::time::Duration;
+
+pub async fn run(args: ListenerArgs) {
+    env_logger::init();
+    loop {
+        info!("Connecting to {}...", args.endpoint.clone());
+        match connect(args.endpoint.clone()).await {
+            Ok(client) => {
+                info!("Connected. Exporting subdags from #{}...", args.from_id);
+                match export(client, args.from_id, args.tx_output.clone()).await {
+                    Ok(_) => {
+                        info!("Exit");
+                        break;
+                    }
+                    Err(e) => {
+                        error!("Failed to export: {}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                error!("Failed to connect: {}", e);
+            }
+        }
+        sleep(Duration::from_secs(1)).await;
+    }
+}
+
+async fn connect(endpoint: String) -> Result<ExporterClient<Channel>, tonic::transport::Error> {
+    ExporterClient::connect(endpoint).await
+}
+
+async fn export(
+    mut client: ExporterClient<Channel>,
+    from_id: u64,
+    tx_output: Option<String>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut stream = client.export(ExportRequest { from_id }).await?.into_inner();
+
+    let mut tx_writer = match tx_output {
+        Some(path) => Some(csv::Writer::from_path(path).unwrap()),
+        None => None,
+    };
+
+    while let Some(subdag) = stream.message().await? {
+        if let Some(ref mut writer) = tx_writer {
+            stats::write_tx_stats(&subdag, writer);
+        }
+
+        let stats = stats::stats(&subdag);
+        if stats.num_txs > 0 {
+            info!(
+                "Received subdag #{} (num txs {}, payload size {}, avg latency {} ms, cert delta {} ms / {} rounds)",
+                subdag.id,
+                stats.num_txs,
+                stats.payload_size,
+                stats.avg_latency,
+                stats.cert_time_delta,
+                stats.cert_round_delta,
+            );
+        } else {
+            info!("Empty subdag #{}", subdag.id,);
+        }
+    }
+
+    info!("Close client");
+    Ok(())
+}
+
+mod stats;
---
+++ simple-benchmark/src/spammer/mod.rs
@@ -0,0 +1,49 @@
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+use log::{error, info};
+use rand::Rng;
+use tokio::time::sleep;
+use tonic::transport::Channel;
+
+use crate::narwhal::{transactions_client::TransactionsClient, Transaction};
+
+use super::SpammerArgs;
+
+pub async fn run(args: SpammerArgs) {
+    env_logger::init();
+    let mut rng = rand::thread_rng();
+    loop {
+        info!("Connecting to {}...", args.endpoint.clone());
+        match connect(args.endpoint.clone()).await {
+            Ok(mut client) => {
+                let payload: Vec<u8> = (1..rng.gen_range(args.min_size..args.max_size))
+                    .map(|_| rng.gen_range(0..255))
+                    .collect();
+                let timestamp = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis();
+                let timestamp_bytes = timestamp.to_be_bytes().to_vec();
+                let transaction = [timestamp_bytes, payload].concat();
+
+                info!(
+                    "Connected. Sending transaction (size {}, timestamp {} ms)",
+                    transaction.len(),
+                    timestamp,
+                );
+                match client.submit_transaction(Transaction { transaction }).await {
+                    Ok(_) => info!("Done. Sleep for {}ms", args.sleep),
+                    Err(e) => error!("Failed to send transaction: {}", e,),
+                }
+            }
+            Err(e) => {
+                error!("Failed to connect: {}", e);
+            }
+        }
+        sleep(Duration::from_millis(args.sleep)).await;
+    }
+}
+
+async fn connect(endpoint: String) -> Result<TransactionsClient<Channel>, tonic::transport::Error> {
+    TransactionsClient::connect(endpoint).await
+}
